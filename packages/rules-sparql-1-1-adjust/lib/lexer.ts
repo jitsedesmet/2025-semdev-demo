fb7ce3037e3a0cef1e22093b1a5bbb704d237142
Jitse De Smet <jitse@reedonk8.be>
2025-09-02 21:12:23 +0200

    adjust is case insensitive

--- packages/rules-sparql-1-1-adjust/lib/lexer.ts
+++ packages/rules-sparql-1-1-adjust/lib/lexer.ts
@@ -1,3 +1,4 @@
+/* eslint-disable require-unicode-regexp */
 import { createToken } from '@traqula/core';
 
-export const BuiltInAdjust = createToken({ name: 'BuiltInAdjust', pattern: 'ADJUST' });
+export const BuiltInAdjust = createToken({ name: 'BuiltInAdjust', pattern: /adjust/i });
